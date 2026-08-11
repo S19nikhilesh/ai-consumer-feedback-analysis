2491ac9174bae9e86a84d2580911f4f2ad2eb7ab
S19nikhilesh <nikhileshsharma2005@gmail.com>
2026-08-11 18:06:59 +0530

    Initial React and Parcel setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>FeedbackAI</h1>
+      <p>AI-Based Consumer Feedback Analysis System</p>
+    </div>
+  );
+};
+
+export default App;
